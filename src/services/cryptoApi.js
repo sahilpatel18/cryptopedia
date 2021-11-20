9b3d40c8fd97fe1e35339013c1de4cd8b3ff25bd
sahilpatel18 <sahil.41801@gmail.com>
2021-11-20 16:54:20 -0500

    configured redux store using redux toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,10 @@
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '4277fc80e8msh8330db94c58326cp15ad9djsn4df85fec4cc7'
+//     }
+//   };
+  
