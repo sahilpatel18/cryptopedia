3b4723c88a9258fd3db772ef80b6d57d57ad1f32
sahilpatel18 <sahil.41801@gmail.com>
2021-11-20 17:33:18 -0500

    removed stat query from base url

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/stats',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': '4277fc80e8msh8330db94c58326cp15ad9djsn4df85fec4cc7'
-//     }
-//   };
-  
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "4277fc80e8msh8330db94c58326cp15ad9djsn4df85fec4cc7",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
