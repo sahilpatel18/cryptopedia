19d679bfd3f5337f59e11c91687bec3d5a301b3c
sahilpatel18 <sahil.41801@gmail.com>
2021-11-20 16:56:23 -0500

    configured redux store using redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import 'antd/dist/antd.css'
+import { Provider } from "react-redux";
+import store from "./app/store";
+import "antd/dist/antd.css";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.querySelector("#root")
 );
